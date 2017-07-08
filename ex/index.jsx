ac9684463d8cf3e09978a27c262cb86b66372937
Eduardo Lopes <edulopestj@gmail.com>
2017-07-08 20:20:25 -0300

    add redux

--- ex/index.jsx
+++ ex/index.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import ReactDom  from 'react-dom'
+import Redux from 'redux'
 import Field from './field'
 
-ReactDom.render(<Field initialValue="text" />, document.getElementById('app'));
+ReactDom.render(
+    <Field initialValue="text" />, 
+    document.getElementById('app'));
