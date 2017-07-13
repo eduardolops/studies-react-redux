4948e97345181c30f00ff17f7ffc7e340f2cbb53
Eduardo Lopes <edulopestj@gmail.com>
2017-07-13 15:32:16 -0300

    add elements in the component

--- ex/index.jsx
+++ ex/index.jsx
@@ -3,15 +3,15 @@ import ReactDom  from 'react-dom'
 import { combineReducers, createStore } from 'redux'
 import { Provider } from 'react-redux'
 
-import Field from './field'
-import FieldReducer from './fieldReducer'
+import Counter from './counter'
+import counterReducer from './counterReducer'
 
 const reducers = combineReducers({
-    field: FieldReducer
+    counter: counterReducer
 })
 
 ReactDom.render(
     <Provider store={createStore(reducers)}>
-        <Field initialValue="text" />
+        <Counter />
     </Provider>,
     document.getElementById('app'));
